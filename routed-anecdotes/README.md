bdd8384c460fc49f79ff73559272f7f166ac46c2
David K <david.king@zoho.com>
2020-07-11 22:42:51 +0100

    7.1: routed anecdotes, step1

---
+++ routed-anecdotes/README.md
@@ -0,0 +1,11 @@
+## Full Stack Open 2019/2020
+
+## Part 7 - Routed Anecdotes
+
+**Commit 1** - 7.1: routed anecdotes, step1
+ - Added Routing and Link
+ - changed order of Route so that home '/' is last
+
+
+
+
--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -1,4 +1,8 @@
 import React, { useState } from 'react'
+import {
+  BrowserRouter as Router,
+  Switch, Route, Link
+} from "react-router-dom"
 
 const Menu = () => {
   const padding = {
@@ -6,9 +10,9 @@ const Menu = () => {
   }
   return (
     <div>
-      <a href='#' style={padding}>anecdotes</a>
-      <a href='#' style={padding}>create new</a>
-      <a href='#' style={padding}>about</a>
+      <Link style={ padding } to='/'>anecdotes</Link>
+      <Link style={ padding } to='/create'>create new</Link>
+      <Link style={ padding } to='/about'>about</Link>
     </div>
   )
 }
@@ -17,7 +21,13 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {
+        anecdotes.map(anecdote => 
+          <li key={ anecdote.id } >
+            <Link to={`/anecdotes/${ anecdote.id }`}>{ anecdote.content }</Link>
+          </li>
+        )
+      }
     </ul>
   </div>
 )
@@ -125,11 +135,21 @@ const App = () => {
   return (
     <div>
       <h1>Software anecdotes</h1>
-      <Menu />
-      <AnecdoteList anecdotes={anecdotes} />
-      <About />
-      <CreateNew addNew={addNew} />
-      <Footer />
+      <Router>
+        <Menu />
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/create">
+            <CreateNew addNew={addNew} />
+          </Route>
+          <Route path="/">
+            <AnecdoteList anecdotes={ anecdotes } />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   )
 }
